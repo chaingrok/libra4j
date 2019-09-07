e3fc37f1aa7f37f975bc4e1860c9e7c0197529ac
chaingrok <chaingrok@gmail.com>
2019-09-07 13:51:04 +0200

    adding ValidatorEnpoint

---
+++ src/main/java/com/chaingrok/libra4j/types/ValidatorEndpoint.java
@@ -0,0 +1,43 @@
+package com.chaingrok.libra4j.types;
+
+import org.libra.grpc.admission_control.AdmissionControlGrpc;
+import org.libra.grpc.admission_control.AdmissionControlGrpc.AdmissionControlBlockingStub;
+import org.libra.grpc.admission_control.AdmissionControlGrpc.AdmissionControlStub;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class ValidatorEndpoint {
+	
+	private String dns = null;
+	private int port = 0;
+	
+	private ManagedChannel channel;
+	private AdmissionControlBlockingStub blockingStub = null;
+	private AdmissionControlStub asyncStub = null;
+	
+	public ValidatorEndpoint(String dns, int port) {
+		this.dns = dns;
+		this.port = port;
+		channel = ManagedChannelBuilder.forAddress(dns,port).usePlaintext().build();
+		blockingStub = AdmissionControlGrpc.newBlockingStub(channel);
+		asyncStub = AdmissionControlGrpc.newStub(channel);
+	}
+
+	public String getDns() {
+		return dns;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public AdmissionControlBlockingStub getBlockingStub() {
+		return blockingStub;
+	}
+
+	public AdmissionControlStub getAsyncStub() {
+		return asyncStub;
+	}
+	
+}
