444470d273347b65f9cc1ae12b3f16ec1e1fbe79
chaingrok <chaingrok@gmail.com>
2019-09-09 22:00:45 +0200

    tests for ValidatorId

---
+++ src/test/java/com/chaingrok/libra4j/test/types/TestValidatorId.java
@@ -0,0 +1,44 @@
+package com.chaingrok.libra4j.test.types;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import com.chaingrok.libra4j.misc.Libra4jError;
+import com.chaingrok.libra4j.misc.Libra4jLog;
+import com.chaingrok.libra4j.test.TestClass;
+import com.chaingrok.libra4j.types.ValidatorId;
+import com.google.protobuf.ByteString;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestValidatorId extends TestClass {
+	
+	@Test
+	public void test001_newInstanceOk() {
+		byte[] id = {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07};
+		ValidatorId validatorId  = new ValidatorId(ByteString.copyFrom(id));
+		assertArrayEquals(id,validatorId.getBytes());
+	}
+	
+	@Test
+	public void test002_newInstanceKo() {
+		assertFalse(Libra4jLog.hasLogs());
+		byte[] id = {0x00};
+		ValidatorId validatorId  = new ValidatorId(ByteString.copyFrom(id));
+		assertArrayEquals(id,validatorId.getBytes());
+		assertTrue(Libra4jLog.hasLogs());
+		ArrayList<Libra4jLog> logs = Libra4jLog.getLogs();
+		assertEquals(1,logs.size());
+		Libra4jLog log = logs.get(0);
+		assertTrue(log instanceof Libra4jError);
+		Libra4jLog.purgeLogs();
+	}
+
+}
