264ef0aa709e2deeb41e843eff1e7555bb37eb8f
chaingrok <chaingrok@gmail.com>
2019-09-11 08:25:39 +0200

    Adding tests for Program

---
+++ src/test/java/com/chaingrok/libra4j/test/types/TestProgram.java
@@ -0,0 +1,23 @@
+package com.chaingrok.libra4j.test.types;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import com.chaingrok.libra4j.test.TestClass;
+import com.chaingrok.libra4j.types.Code;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestProgram extends TestClass {
+	
+	@Test
+	public void test001_newInstance() {
+		Code code = new Code(Code.MINT.getBytes());
+		assertArrayEquals(Code.MINT.getBytes(),code.getBytes());
+		code = new Code(Code.PEER_TO_PEER_TRANSFER.getBytes());
+		assertArrayEquals(Code.PEER_TO_PEER_TRANSFER.getBytes(),code.getBytes());
+	}
+
+}
