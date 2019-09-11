7d194eea1dd92fc6c89a6f7aa28b01e7ef2107d4
chaingrok <chaingrok@gmail.com>
2019-09-11 21:26:52 +0200

    Extending tests for Signature

--- src/test/java/com/chaingrok/libra4j/test/types/TestSignature.java
+++ src/test/java/com/chaingrok/libra4j/test/types/TestSignature.java
@@ -1,5 +1,6 @@
 package com.chaingrok.libra4j.test.types;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -12,19 +13,24 @@ import com.chaingrok.libra4j.misc.Libra4jException;
 import com.chaingrok.libra4j.misc.Utils;
 import com.chaingrok.libra4j.test.TestClass;
 import com.chaingrok.libra4j.types.Signature;
+import com.google.protobuf.ByteString;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestSignature extends TestClass {
 	
 	@Test
 	public void test001_newInstanceOk() {
-		byte[] bytes = Utils.getByteArray(Signature.BYTE_LENGTH,0x00);
+		byte[] bytes = Utils.getByteArray(Signature.BYTE_LENGTH,0x01);
 		Signature signature = new Signature(bytes);
 		assertSame(bytes,signature.getBytes());
+		//
+		ByteString byteString = ByteString.copyFrom(bytes);
+		signature = new Signature(byteString);
+		assertArrayEquals(bytes,signature.getBytes());
 	}
 	
 	@Test
-	public void test001_newInstanceKo() {
+	public void test002_newInstanceKo() {
 		byte[] bytes = Utils.getByteArray(Signature.BYTE_LENGTH-1,0x00);
 		try {
 			new Signature(bytes);
@@ -32,7 +38,13 @@ public class TestSignature extends TestClass {
 		} catch (Libra4jException e) {
 			assertTrue(e.getMessage().startsWith("invalid length for signature"));
 		}
-		
+	}
+	
+	@Test
+	public void test002_SignatureToString() {
+		byte[] bytes = Utils.getByteArray(Signature.BYTE_LENGTH,0x01);
+		Signature signature = new Signature(bytes);
+		assertTrue(signature.toString().contains(Utils.byteArrayToHexString(bytes)));
 	}
 
 }
