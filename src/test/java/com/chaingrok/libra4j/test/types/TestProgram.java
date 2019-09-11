0d34d0fa12462496067679baef1d32f4495ff5ae
chaingrok <chaingrok@gmail.com>
2019-09-11 20:16:57 +0200

    adding tests for Program

--- src/test/java/com/chaingrok/libra4j/test/types/TestProgram.java
+++ src/test/java/com/chaingrok/libra4j/test/types/TestProgram.java
@@ -1,23 +1,41 @@
 package com.chaingrok.libra4j.test.types;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertSame;
+
+import java.util.ArrayList;
 
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 import com.chaingrok.libra4j.test.TestClass;
+import com.chaingrok.libra4j.types.Argument;
 import com.chaingrok.libra4j.types.Code;
+import com.chaingrok.libra4j.types.Module;
+import com.chaingrok.libra4j.types.Program;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestProgram extends TestClass {
 	
 	@Test
 	public void test001_newInstance() {
+		Program program = new Program();
+		//
 		Code code = new Code(Code.MINT.getBytes());
-		assertArrayEquals(Code.MINT.getBytes(),code.getBytes());
-		code = new Code(Code.PEER_TO_PEER_TRANSFER.getBytes());
-		assertArrayEquals(Code.PEER_TO_PEER_TRANSFER.getBytes(),code.getBytes());
+		program.setCode(code);
+		assertSame(code,program.getCode());
+		//
+		Long codeSize = 15L;
+		program.setCodeSize(codeSize);
+		assertSame(codeSize,program.getCodeSize());
+		//
+		ArrayList<Module> modules = new ArrayList<Module>();
+		program.setModules(modules);
+		assertSame(modules,program.getModules());
+		//
+		ArrayList<Argument> arguments = new ArrayList<Argument>();
+		program.setArguments(arguments);
+		assertSame(arguments,program.getArguments());
 	}
 
 }
