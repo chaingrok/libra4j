add28004df4f71b379ada237a952ca51d4aed184
chaingrok <chaingrok@gmail.com>
2019-09-13 09:42:49 +0200

    extend tests for EventData

--- src/test/java/com/chaingrok/libra4j/test/types/TestEventData.java
+++ src/test/java/com/chaingrok/libra4j/test/types/TestEventData.java
@@ -1,6 +1,8 @@
 package com.chaingrok.libra4j.test.types;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -10,6 +12,7 @@ import com.chaingrok.libra4j.misc.Utils;
 import com.chaingrok.libra4j.test.TestClass;
 import com.chaingrok.libra4j.types.AccountAddress;
 import com.chaingrok.libra4j.types.EventData;
+import com.chaingrok.libra4j.types.Path.Type;
 import com.google.protobuf.ByteString;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -17,9 +20,19 @@ public class TestEventData extends TestClass {
 	
 	@Test
 	public void test001NewInstance() {
-		ByteString byteString  = ByteString.copyFrom(Utils.getByteArray(AccountAddress.BYTE_LENGTH * 2)); //length must be > Account address length to deserialize access path;
+		byte[] bytes = Utils.getByteArray(AccountAddress.BYTE_LENGTH * 2,0x35); //length must be > Account address length to deserialize access path;
+		ByteString byteString  = ByteString.copyFrom(bytes); 
 		EventData eventData = new EventData(byteString);
 		assertEquals(byteString,eventData.getByteString());
+		String string = eventData.toString();
+		assertNotNull(string);
+		assertTrue(string.length() > 0);
+		assertTrue(string.contains(Utils.byteArrayToHexString(bytes)));
+		//with type
+		string = eventData.toString(Type.UNKNOWN);
+		assertNotNull(string);
+		assertTrue(string.length() > 0);
+		assertTrue(string.contains(Utils.byteArrayToHexString(bytes)));
 	}
 
 }
