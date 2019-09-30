aa108241824d76d70a99a64bed706a714a0f28ce
chaingrok <chaingrok@gmail.com>
2019-09-30 06:21:06 +0200

    Adding UInt16 and first tests

---
+++ src/main/java/com/chaingrok/libra4j/types/UInt16.java
@@ -0,0 +1,34 @@
+package com.chaingrok.libra4j.types;
+
+import java.math.BigInteger;
+
+import com.google.protobuf.ByteString;
+
+public class UInt16 extends UInt {
+	
+	public static final int BYTE_LENGTH = 2;
+	public static final BigInteger MAX_VALUE = new BigInteger("65535"); //65 535 
+
+	public UInt16(ByteString byteString) {
+		super(byteString);
+	}
+	
+	public UInt16(byte[] bytes) {
+		super(bytes);
+	}
+	
+	public UInt16(Long value) {
+		super(value);
+	}
+
+	@Override
+	protected int getLength() {
+		return BYTE_LENGTH ;
+	}
+	
+	@Override
+	protected BigInteger getMaxValue() {
+		return MAX_VALUE;
+	}
+
+}
