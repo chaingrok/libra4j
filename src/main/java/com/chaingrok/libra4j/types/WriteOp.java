f72116db5940a2778fbabcb57c35fa30ba0e00b0
chaingrok <chaingrok@gmail.com>
2019-10-04 14:35:46 +0200

    initial tests for WriteOp

--- src/main/java/com/chaingrok/libra4j/types/WriteOp.java
+++ src/main/java/com/chaingrok/libra4j/types/WriteOp.java
@@ -1,11 +1,17 @@
 package com.chaingrok.libra4j.types;
 
-import java.util.ArrayList;
+import com.chaingrok.libra4j.misc.LCSInterface;
+import com.chaingrok.libra4j.misc.LCSProcessor;
+import com.chaingrok.libra4j.misc.Libra4jError;
 
-public class WriteOp {
+public class WriteOp extends ByteArray implements LCSInterface {
 	
 	private Boolean isValue = null;
-	private ArrayList<UInt8> value = null;
+	private Type type = null;
+	
+	public WriteOp(byte[] bytes) {
+		super(bytes);
+	}
 	
 	public Boolean getIsValue() {
 		return isValue;
@@ -15,12 +21,68 @@ public class WriteOp {
 		this.isValue = isValue;
 	}
 	
-	public ArrayList<UInt8> getValue() {
-		return value;
+	public void setType(Type type) {
+		this.type = type;
 	}
 	
-	public void setValue(ArrayList<UInt8> value) {
-		this.value = value;
+	public Type getType() {
+		return type;
+	}
+	
+	public enum Type implements LCSInterface {
+		
+		WRITE(0),
+		DELETE(1),
+		UNRECOGNIZED(-1)
+		
+		;
+		
+		int opType = -1;
+		
+		private Type(int opType) {
+			this.opType = opType;
+		}
+		
+		public int getOpType() {
+			return opType;
+		}
+		
+		public static Type get(int opType) {
+			Type result = null;
+			for (Type type : Type.values()) {
+				if (type.opType == opType) {
+					result = type;
+					break;
+				}
+			}
+			if (result == Type.UNRECOGNIZED) {
+				new Libra4jError(Libra4jError.Type.UNKNOWN_VALUE, "type: " + Type.UNRECOGNIZED + "found");
+			}
+			if (result == null) {
+				new Libra4jError(Libra4jError.Type.UNKNOWN_VALUE, "unrecognized opType: " + opType);
+				result = Type.UNRECOGNIZED;
+			}
+			return result;
+		}
+		
+		
+		@Override
+		public LCSProcessor encodeToLCS(LCSProcessor lcsProcessor) {
+			UInt32 uint32 = null;
+			uint32 = new UInt32(this.getOpType());
+			lcsProcessor.encode(uint32);
+			return lcsProcessor;
+		}
+
+	}
+
+	@Override
+	public LCSProcessor encodeToLCS(LCSProcessor lcsProcessor) {
+		UInt32 uint32 = null;
+		uint32 = new UInt32(getType().getOpType());
+		lcsProcessor.encode(uint32);
+		lcsProcessor.encode(getBytes());
+		return lcsProcessor;
 	}
 	
 }
---
+++ src/test/java/com/chaingrok/libra4j/test/types/TestVWriteSet.java
@@ -0,0 +1,41 @@
+package com.chaingrok.libra4j.test.types;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import com.chaingrok.libra4j.misc.Libra4jLog;
+import com.chaingrok.libra4j.test.TestClass;
+import com.chaingrok.libra4j.types.WriteOp;
+import com.chaingrok.libra4j.types.WriteOp.Type;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestVWriteSet extends TestClass {
+	
+	@Test
+	public void test001WriteOptype() {
+		assertEquals(Type.WRITE,Type.get(Type.WRITE.getOpType()));
+		assertEquals(Type.DELETE,Type.get(Type.DELETE.getOpType()));
+		assertFalse(Libra4jLog.hasLogs());
+		assertEquals(Type.UNRECOGNIZED,Type.get(Type.UNRECOGNIZED.getOpType()));
+		assertTrue(Libra4jLog.hasLogs());
+		Libra4jLog.purgeLogs();
+	}
+	
+	@Test
+	public void test002WriteOpInstanceOk() {
+		byte[] bytes = {0x00,0x01};
+		WriteOp writeOp = new WriteOp(bytes);
+		assertSame(bytes,writeOp.getBytes());
+		assertNull(writeOp.getIsValue());
+		writeOp.setIsValue(true);
+		assertTrue(writeOp.getIsValue());
+	}
+
+}
