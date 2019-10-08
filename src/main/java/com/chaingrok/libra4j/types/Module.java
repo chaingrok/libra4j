22fd1ff2618e0f0c09855b0023e2c20db4b9da67
chaingrok <chaingrok@gmail.com>
2019-10-08 04:47:00 +0200

    Adding LCS interface to Module

--- src/main/java/com/chaingrok/libra4j/types/Module.java
+++ src/main/java/com/chaingrok/libra4j/types/Module.java
@@ -1,9 +1,18 @@
 package com.chaingrok.libra4j.types;
 
-public class Module extends ByteArray {
+import com.chaingrok.libra4j.misc.LCSInterface;
+import com.chaingrok.libra4j.misc.LCSProcessor;
+
+public class Module extends ByteArray implements LCSInterface {
 
 	public Module(byte[] bytes) {
 		super(bytes);
+	}
+
+	@Override
+	public LCSProcessor encodeToLCS(LCSProcessor lcsProcessor) {
+		lcsProcessor.encode(bytes);
+		return lcsProcessor;
 	}
 
 }
