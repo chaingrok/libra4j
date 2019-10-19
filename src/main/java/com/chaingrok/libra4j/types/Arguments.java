3befb1265bbc4583636617f08d66603fc125dde5
chaingrok <chaingrok@gmail.com>
2019-10-19 08:09:21 +0200

    add set() for cascade

--- src/main/java/com/chaingrok/libra4j/types/Arguments.java
+++ src/main/java/com/chaingrok/libra4j/types/Arguments.java
@@ -4,4 +4,9 @@ import java.util.ArrayList;
 
 @SuppressWarnings("serial")
 public class Arguments extends ArrayList<Argument>{
+	
+	public Arguments set(Argument argument) {
+		super.add(argument);
+		return this;
+	}
 }
