d8629d92ebbb07fa2338e7f38ad3c86e2a9c8c52
chaingrok <chaingrok@gmail.com>
2019-10-21 07:12:40 +0200

    make Signature extend ByteArray

--- src/main/java/com/chaingrok/libra4j/types/Signature.java
+++ src/main/java/com/chaingrok/libra4j/types/Signature.java
@@ -3,19 +3,19 @@ package com.chaingrok.libra4j.types;
 import com.chaingrok.lib.ChaingrokError;
 import com.chaingrok.lib.Utils;
 import com.chaingrok.lib.ChaingrokLog.Type;
+
 import com.google.protobuf.ByteString;
 
-public class Signature {
+public class Signature extends ByteArray {
 	
 	public static final int BYTE_LENGTH = 64;
-	
-	private byte[] bytes = null;
 	
 	public Signature(ByteString byteString) {
 		this(byteString.toByteArray());
 	}
 	
 	public Signature(byte[] bytes) {
+		super(bytes);
 		if (bytes == null) {
 			new ChaingrokError(Type.NULL_DATA,"input byte array cannot be null for signature");
 		} else {
@@ -23,11 +23,6 @@ public class Signature {
 				new ChaingrokError(Type.INVALID_LENGTH,"invalid length for signature: " + bytes.length + " <> " + BYTE_LENGTH + " (" + Utils.byteArrayToHexString(bytes) + ")");
 			}
 		}
-		this.bytes = bytes;
-	}
-
-	public byte[] getBytes() {
-		return bytes;
 	}
 	
 	public boolean isValid() {
@@ -37,12 +32,4 @@ public class Signature {
 		}
 		return result;
 	}
-	
-	@Override
-	public String toString() {
-		String result = "";
-		result += Utils.byteArrayToHexString(bytes);
-		return result;
-	}
-
 }
